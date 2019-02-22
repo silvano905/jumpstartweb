d744f32c0a3baebf574ff73680e66af1ce920146
silvano valdez <silvanovaldez90@gmail.com>
2019-02-21 20:50:48 -0600

    5

--- jumpstart/urls.py
+++ jumpstart/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include, re_path
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
+
 from . import views
 app_name = 'jumpstart'
 
@@ -7,5 +9,7 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('car-not-starting/', views.car_not_starting, name='car-not-starting'),
     path('dead-car-battery/', views.dead_battery, name='dead-car-battery'),
+    path('robots.txt', TemplateView.as_view(template_name="jumpstart/robots.txt", content_type="text/plain"),
+         name="robots_file")
 
 ]
