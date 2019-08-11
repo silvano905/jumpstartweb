b6ca5b28a9ae6ab4c622a83b442145f7e555e097
silvano valdez <silvanovaldez90@gmail.com>
2019-08-10 22:32:41 -0500

    sissss

--- jumpstartservice/settings.py
+++ jumpstartservice/settings.py
@@ -47,12 +47,24 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CACHE_MIDDLEWARE_SECONDS= 20000
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
 ROOT_URLCONF = 'jumpstartservice.urls'
 
@@ -141,3 +153,4 @@ EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'from jump'
+
